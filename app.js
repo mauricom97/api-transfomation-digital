b52b16dc84d52e94ac45c95b8309c66e0fb49124
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2021-11-05 13:08:55 -0200

    PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3352
+const port = process.env.PORT || 3352
 const bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
